295ddd902e9acd1ef31dc2ac8a168368864bc8fe
dlra <anselldlr@gmail.com>
2020-02-24 10:34:29 +0000

    Add Savings Account class

---
+++ TechnicalInterview/SavingsAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TechnicalInterview
+{
+    public class SavingsAccount
+    {
+        public string PersonName { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime LastWithdrawalDate { get; set; }
+ 
+        public void Deposit(decimal amount)
+        {
+            var tempValue = Balance;
+            var newValue = tempValue + amount;
+            Balance = newValue;
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (LastWithdrawalDate.Date == DateTime.Now.Date)
+                throw new AlreadyWithdrawnTodayException();
+            
+            var tempValue = Balance;
+            var newValue = tempValue - amount;
+            if (newValue < 0)
+                throw new AccountOverdrawnException();
+
+            Balance = newValue;
+        }
+    }
+}
